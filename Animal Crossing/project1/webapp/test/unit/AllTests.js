0b5eca44ca17dc3b8a4276a565f25b07a7148a69
Adnan Khan <adnan.khan2019@vitbhopal.ac.in>
2024-01-22 23:26:41 +0530

    Updating + adding new Card Image Hover effect

---
+++ Animal Crossing/project1/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"project1/test/unit/controller/TestView1.controller"
+], function () {
+	"use strict";
+});
---
+++ Animal Crossing/tailwind.config.js
@@ -0,0 +1,27 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      borderRadius: {
+        "6xl": "3rem",
+        "8xl": "4rem",
+      },
+      colors: {
+        primary: "# ",
+        neutral: {
+          100: "#FFF7DF",
+          200: "#FAEFD4",
+          800: "#805120",
+          900: "#664624",
+        },
+      },
+      fontFamily: {
+        rounded: [
+          "ui-rounded",
+          "Nunito",
+          "-apple-system",
+          "BlinkMacSystemFontt",
+        ],
+      },
+    },
+  },
+};
